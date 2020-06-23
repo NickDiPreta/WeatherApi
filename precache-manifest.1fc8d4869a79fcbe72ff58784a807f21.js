c221c8c60846fc92ee044a6e51c70fdbecccf945
nick <nicholasdipreta2020@gmail.com>
2020-06-22 23:52:47 -0400

    Updates

---
+++ precache-manifest.1fc8d4869a79fcbe72ff58784a807f21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89ce190f30e412d71ae60356d7d0a3ba",
+    "url": "/WeatherApi/index.html"
+  },
+  {
+    "revision": "3470c8e98846998d03e1",
+    "url": "/WeatherApi/static/css/main.618e0585.chunk.css"
+  },
+  {
+    "revision": "5e833a13b7f4d0fa1490",
+    "url": "/WeatherApi/static/js/2.b02f7cae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherApi/static/js/2.b02f7cae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3470c8e98846998d03e1",
+    "url": "/WeatherApi/static/js/main.8a2a290e.chunk.js"
+  },
+  {
+    "revision": "a9623d63119334ff0a6c",
+    "url": "/WeatherApi/static/js/runtime-main.e76acc8b.js"
+  }
+]);
